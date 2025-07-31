fc3aadac7cadfa3412445a70565298f7ab0a92be
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2025-07-31 18:56:54 +0900

    chore: お遊びuseState

---
+++ src/test.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import type { Dispatch, SetStateAction } from 'react';
+
+function ClickCounter({
+  count,
+  setCount
+}: {
+  count: number;
+  setCount: Dispatch<SetStateAction<number>>;
+}) {
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(prev => prev + 1)}>Increment</button>
+    </div>
+  );
+}
+
+function EvenOrOdd({ count }: { count: number }) {
+  return (
+    <div>
+      <p>{ count % 2 === 0 ? "Even" : "Odd" }</p>
+    </div>
+  );
+}
+
+export function Test() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <ClickCounter count={count} setCount={setCount} />
+      <EvenOrOdd count={count} />
+      <p>Current count is: {count}</p>
+    </div>
+  );
+}
+
